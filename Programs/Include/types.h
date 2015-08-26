80a92cf7a45d230a3ca277f120481dea2fb738db
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-08-26 17:33:59 +0900

    Updated

--- Programs/Include/types.h
+++ Programs/Include/types.h
@@ -1,8 +1,32 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
+#define NO_ERROR 0
+
+typedef int status_t;
+typedef unsigned int size_t;
+
+typedef char s8;
+typedef unsigned char u8;
+
+typedef short int s16;
 typedef unsigned short int u16;
+
 typedef long int s32;
+typedef unsigned long int u32;
+
+typedef long long int s64;
+typedef unsigned long long int u64;
+
+typedef float  f32;
 typedef double f64;
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 1
+#endif
 
 #endif // #ifndef _TYPES_H_
