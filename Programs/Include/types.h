281b3ed4915f0fcadf718225c30d276c5bb855fa
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-09-01 17:32:31 +0900

    Moved some declarations to types.h

--- Programs/Include/types.h
+++ Programs/Include/types.h
@@ -27,6 +27,14 @@ typedef double f64;
 #define FALSE 1
 #endif
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif // #ifndef M_PI
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif // NULL
+
 #define PTR_CAST(t, v) reinterpret_cast<t>(v)
 
 #endif // #ifndef _TYPES_H_
