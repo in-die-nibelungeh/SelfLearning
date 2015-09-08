8a22d9b241f0f7900d0f48845218b03172a6a33e
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-09-08 17:34:24 +0900

    Added, but Kaiser is not verified yet

--- Programs/Window/Window.cpp
+++ Programs/Window/Window.cpp
@@ -1,39 +1,45 @@
 #include <math.h>
 
-#include "types.h"
 #include "Buffer.h"
+#include "Window.h"
 
 #define POW2(x) ((x)*(x))
+
 
 namespace masp {
 namespace window {
 
-double g_Pi(M_PI);
-
-/*
-void GeneralizedHamming(Container::Vector<double>& w)
-        0.5 =< a =< 1.0
-        0.50: Hanning
-        0.54: Hamming
-        1.00:  Square
-        w[i] = a - (1 - a) * cos( (i+o) * dt);
-
-*/
-
-double HammingFunction(int i, double dt, double arg)
+enum WindowFunctionId
 {
-    return 0.54 - 0.46 * cos( (2*i+1) * dt / 2.0);
-}
+    WFI_HANNING = 1,
+    WFI_HAMMING,
+    WFI_GENERALIZED_HAMMING,
+    WFI_BLACKMAN,
+    WFI_BLACKMAN_HARRIS,
+    WFI_NUTTALL,
+    WFI_KAISER
+};
 
-double HanningFunction(int i, double dt, double arg)
-{
-    return 0.5 - 0.5 * cos( (2*i+1) * dt / 2.0);
-}
+static const double g_Pi(M_PI);
+
+typedef double (*WindowFunctionType)(int i, double dt, double arg) ;
 
 double GeneralizedHammingFunction(int i, double dt, double arg)
 {
     double& a = arg;
     return a - (1.0 - a) * cos( (2*i+1) * dt / 2.0);
+}
+
+double HammingFunction(int i, double dt, double arg)
+{
+    //return 0.54 - 0.46 * cos( (2*i+1) * dt / 2.0);
+    return GeneralizedHammingFunction(i, dt, 0.54);
+}
+
+double HanningFunction(int i, double dt, double arg)
+{
+    //return 0.5 - 0.5 * cos( (2*i+1) * dt / 2.0);
+    return GeneralizedHammingFunction(i, dt, 0.50);
 }
 
 double BlackmanFunction(int i, double dt, double arg)
@@ -46,67 +52,87 @@ double BlackmanHarrisFunction(int i, double dt, double arg)
     return 0.35875 - 0.48829 * cos( (2*i+1) * dt / 2.0) + 0.14128 * cos( (2*i+1) * dt) - 0.01168 * cos((2*i+1) * dt * 1.5);
 }
 
+double Factorial(int n)
+{
+    double ans = 1.0;
+    for (int i = 1; i <= n; ++i) { ans *= i; }
+    return ans;
+}
+
 double I0(double x)
 {
-    return 1.0;
+    double ans = 0.0;
+    for (int k = 0; k < 1000; ++k)
+    {
+        ans += pow(x/2, 2 * k) / POW2(Factorial(k));
+    }
+    return ans;
 }
 
-double Kaiser(int i, double dt, double arg)
+double KaiserFunction(int i, double dt, double a)
 {
-    double& a = arg;
-    return I0 ( g_Pi * a * sqrt(1 - POW2((2*i+1) * dt / g_Pi / 2.0 - 1) ) ) / I0 (g_Pi * a);
+    return I0( g_Pi * a * sqrt(1 - POW2((2*i+1) * dt / g_Pi / 2.0 - 1) ) ) / I0 (g_Pi * a);
 }
 
-double Nuttall(int i, double dt, double arg)
+double NuttallFunction(int i, double dt, double arg)
 {
     return 0.355768 - 0.487396 * cos( (2*i+1) * dt / 2.0) + 0.144232 * cos( (2*i+1) * dt) - 0.012604 * cos((2*i+1) * dt * 1.5);
 }
 
-void GeneralizedHamming(double a, double w[], size_t N)
+WindowFunctionType GetWindowFunction(int type)
 {
+    WindowFunctionType function = reinterpret_cast<WindowFunctionType>(NULL);
+    switch(type)
+    {
+    case WFI_HANNING:             function = HanningFunction; break;
+    case WFI_HAMMING:             function = HammingFunction; break;
+    case WFI_GENERALIZED_HAMMING: function = GeneralizedHammingFunction; break;
+    case WFI_BLACKMAN:            function = BlackmanFunction; break;
+    case WFI_BLACKMAN_HARRIS:     function = BlackmanHarrisFunction; break;
+    case WFI_NUTTALL:             function = NuttallFunction; break;
+    case WFI_KAISER:              function = KaiserFunction; break;
+    }
+    return function;
+}
+
+void GenerateWindow(double w[], size_t N, int type, double arg)
+{
+    WindowFunctionType function = GetWindowFunction(type);
+    ASSERT(function != NULL);
     double dt = 2 * g_Pi / N;
     for (int i = 0; i < N; ++i)
     {
-        w[i] = a - (1.0 - a) * cos( (2*i+1) * dt / 2.0);
+        w[i] = function(i, dt, arg);
     }
 }
 
-void GeneralizedHamming(double a, Container::Vector<double>& w)
-{
-    const size_t N = w.GetNumOfData();
-    double dt = 2 * g_Pi / N;
-    for (int i = 0; i < N; ++i)
-    {
-        w[i] = a - (1.0 - a) * cos( (2*i+1) * dt / 2.0);
-    }
-}
-
-void Hanning(double w[], size_t N)         { GeneralizedHamming(0.50, w, N); }
-void Hanning(Container::Vector<double>& w) { GeneralizedHamming(0.50, w); }
-void Hamming(double w[], size_t N)         { GeneralizedHamming(0.54, w, N); }
-void Hamming(Container::Vector<double>& w) { GeneralizedHamming(0.54, w); }
-
-/*
-void Hanning(double w[], size_t N)
-{
-    double dt = 2 * g_Pi / N;
-    for (int i = 0; i < N; ++i)
-    {
-        w[i] = 0.5 - 0.5 * cos( (2*i+1) * dt / 2.0);
-    }
-    return ;
-}
-
-void Hanning(Container::Vector<double>& w)
+void GenerateWindow(Container::Vector<double>& w, int type, double arg)
 {
     const size_t N = w.GetNumOfData();
+    WindowFunctionType function = GetWindowFunction(type);
+    ASSERT(function != NULL);
+
     double dt = 2 * g_Pi / N;
     for (int i = 0; i < N; ++i)
     {
-        w[i] = 0.5 - 0.5 * cos( (2*i+1) * dt / 2.0);
+        w[i] = function(i, dt, arg);
     }
 }
-*/
+
+void Hanning(double w[], size_t N)          { GenerateWindow(w, N, WFI_HANNING , 0.0); }
+void Hanning(Container::Vector<double>& w)  { GenerateWindow(w,    WFI_HANNING , 0.0); }
+void Hamming(double w[], size_t N)          { GenerateWindow(w, N, WFI_HAMMING , 0.0); }
+void Hamming(Container::Vector<double>& w)  { GenerateWindow(w,    WFI_HAMMING , 0.0); }
+void GeneralizedHamming(double w[], size_t N, double a)          { GenerateWindow(w, N, WFI_GENERALIZED_HAMMING , a); }
+void GeneralizedHamming(Container::Vector<double>& w, double a)  { GenerateWindow(w,    WFI_GENERALIZED_HAMMING , a); }
+void Blackman(double w[], size_t N)         { GenerateWindow(w, N, WFI_BLACKMAN, 0.0); }
+void Blackman(Container::Vector<double>& w) { GenerateWindow(w,    WFI_BLACKMAN, 0.0); }
+void BlackmanHarris(double w[], size_t N)         { GenerateWindow(w, N, WFI_BLACKMAN_HARRIS, 0.0); }
+void BlackmanHarris(Container::Vector<double>& w) { GenerateWindow(w,    WFI_BLACKMAN_HARRIS, 0.0); }
+void Nuttall(double w[], size_t N)          { GenerateWindow(w, N, WFI_NUTTALL, 0.0); }
+void Nuttall(Container::Vector<double>& w)  { GenerateWindow(w,    WFI_NUTTALL, 0.0); }
+void Kaiser(double w[], size_t N, double a)           { GenerateWindow(w, N, WFI_KAISER, a); }
+void Kaiser(Container::Vector<double>& w, double a)   { GenerateWindow(w,    WFI_KAISER, a); }
 
 } // namespace window {
 } // namespace masp {
--- Programs/Window/main.cpp
+++ Programs/Window/main.cpp
@@ -10,19 +10,40 @@ static void test_hanning(void)
     int StopBand = 1500;
     int TransferBandWidth = StopBand - PassBand;
     int cutOff = PassBand + TransferBandWidth/2;
-
     double coef[256];
     size_t N = 24;
+
+    Container::Vector<double> han(N), ham(N), b(N), bh(N), n(N), k(N);
     masp::window::Hanning(coef, N);
+    masp::window::Hanning(han);
+    masp::window::Hamming(ham);
+    masp::window::Blackman(b);
+    masp::window::BlackmanHarris(bh);
+    masp::window::Nuttall(n);
+    masp::window::Kaiser(k, 3.0);
+    printf(",Hanning,Hamming,Blackman,BlackmanHarris,Nuttall,Kaiser\n");
     for (int i = 0; i < N; ++i)
     {
-        printf("N=%d: %d,%f\n", N, i, coef[i]);
+        printf("%d,%f,%f,%f,%f,%f,%f\n", i, han[i], ham[i], b[i], bh[i], n[i], k[i]);
     }
     N = 25;
+    han.Reallocate(N);
+    ham.Reallocate(N);
+    b.Reallocate(N);
+    bh.Reallocate(N);
+    n.Reallocate(N);
+    k.Reallocate(N);
     masp::window::Hanning(coef, N);
+    masp::window::Hanning(han);
+    masp::window::Hamming(ham);
+    masp::window::Blackman(b);
+    masp::window::BlackmanHarris(bh);
+    masp::window::Nuttall(n);
+    masp::window::Kaiser(k, 3.0);
+    printf(",Hanning,Hamming,Blackman,BlackmanHarris,Nuttall,Kaiser\n");
     for (int i = 0; i < N; ++i)
     {
-        printf("N=%d: %d,%f\n", N, i, coef[i]);
+        printf("%d,%f,%f,%f,%f,%f,%f\n", i, han[i], ham[i], b[i], bh[i], n[i], k[i]);
     }
     return ;
 }
