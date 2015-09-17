b16430d9353bb096c000e31bc4b9f5536108c03c
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-09-18 07:37:40 +0900

    * Updated

--- Programs/Include/debug.h
+++ Programs/Include/debug.h
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Ryosuke Kanata
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
@@ -26,13 +50,17 @@
     } while (0)
 
 
+#define CHECK_VALUE(var, ans)  \
+    DEBUG_LOG("[%s] " #var"=%g (ans=%g)\n", \
+        (var)==(ans) ? "OK" : "NG", static_cast<double>(var), static_cast<double>(ans))
+
 #define CHECK_VALUE_INT(var, ans)  \
     DEBUG_LOG("[%s] " #var"=%d (ans=%d)\n", \
-        (var)==(ans) ? "OK" : "NG", var, ans)
+        (var)==(ans) ? "OK" : "NG", var, static_cast<int>(ans))
 
 #define CHECK_VALUE_FLT(var, ans)  \
     DEBUG_LOG("[%s] " #var"=%f (ans=%f)\n", \
-        (var)==(ans) ? "OK" : "NG", var, ans)
+        (var)==(ans) ? "OK" : "NG", var, static_cast<double>(ans))
 
 #ifndef ASSERT
 #define ASSERT(c) \
