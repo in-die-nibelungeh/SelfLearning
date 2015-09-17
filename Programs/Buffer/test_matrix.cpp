adabc0065e4793a9b7a9222a20bd02a0c22f1c2f
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-09-17 13:02:25 +0900

    Added to test mcon::Matrix

---
+++ Programs/Buffer/test_matrix.cpp
@@ -0,0 +1,21 @@
+
+static void test_matrix(void)
+{
+    int length = 6;
+    mcon::Vector<double> dvec(length);
+    for (int i = 0; i < length; ++i)
+    {
+        dvec[i] = i + 1;
+    }
+    mcon::Matrix<double> m(1,1);
+
+    m = dvec.Transpose();
+
+    CHECK_VALUE(m.GetNumOfArray(), length);
+    CHECK_VALUE(m.GetNumOfData(), 1);
+
+    for (int i = 0; i < length; ++i)
+    {
+        CHECK_VALUE(m[i][0], i+1);
+    }
+}
