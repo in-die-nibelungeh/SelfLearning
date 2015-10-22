a8d006e2fbe10b263930fe944eb4a733de19cc8d
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-10-22 17:02:07 +0900

    * Change output directory of object files * Added checking some variables * Changed install directory of header files

--- Build/Make/modulerules.mk
+++ Build/Make/modulerules.mk
@@ -1,5 +1,16 @@
 
-BIN ?= a.exe
+BIN ?= 
+NOECHO ?= 
+
+ifndef MY_AUDIO_ROOT
+$(error Define MY_AUDIO_ROOT)
+endif
+
+ifdef LIB
+ifndef MODULE_NAME
+$(error Define MODULE_NAME)
+endif
+endif
 
 CC := $(TOOLCHAIN_PREFIX)gcc.exe
 LD := $(TOOLCHAIN_PREFIX)ld.exe
@@ -8,6 +19,8 @@ LIBS += -lstdc++ -lm
 
 PROJECT_INCDIR := $(MY_AUDIO_ROOT)/Programs/Include
 PROJECT_LIBDIR := $(MY_AUDIO_ROOT)/Programs/Library
+
+MODULE_HEADER_INSTALL_DIR := $(PROJECT_INCDIR)/$(MODULE_NAME)
 
 INCDIRS += $(PROJECT_INCDIR)
 LIBDIRS += $(PROJECT_LIBDIR)
@@ -44,6 +57,10 @@ CPPFLAGS += \
 	$(addprefix -L,$(LIBDIRS)) \
 	$(WARNINGS) \
 
+# Macros
+# Mkdir=if not exist @1 mkdir @1
+Mkdir=if [ ! -d "$(1)" ] ; then mkdir -p "$(1)" ; fi
+
 all: $(BIN)
 	@echo Succeeded in building $(BIN).
 
@@ -70,12 +87,13 @@ install: $(LIB) $(MODULE_HEADER)
 		echo cp $(LIB) $(PROJECT_LIBDIR)/ ; \
 		cp $(LIB) $(PROJECT_LIBDIR)/ ; \
 	fi
-	cp $(MODULE_HEADER) $(PROJECT_INCDIR)/
+	$(NOECHO)$(call Mkdir, $(MODULE_HEADER_INSTALL_DIR))
+	cp $(MODULE_HEADER) $(MODULE_HEADER_INSTALL_DIR)/
 
 clean: 
 	@rm -rf $(BIN) $(LIB) *.BAK *.o
 
 .cpp.o: $(INC)
-	$(CC) -c $(CPPFLAGS) $<
+	$(CC) -c $(CPPFLAGS) $< -o $(<:.cpp=.o)
 
 .PHONY: all lib install clean build run
