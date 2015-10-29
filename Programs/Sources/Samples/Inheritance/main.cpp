ea949279026dcdd095c3f1bb17a202ea3511f882
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-10-29 17:22:43 +0900

    Added as sample for class inheritance in c++

---
+++ Programs/Sources/Samples/Inheritance/main.cpp
@@ -0,0 +1,75 @@
+
+#include <new>
+#include <stdio.h>
+#include <stdlib.h>
+
+class VectorBase
+{
+     friend class Vector;
+public:
+     VectorBase(int n)
+     : m_AddressAligned(NULL), m_Length(n)
+     {
+         printf("%s\n", __func__);
+     }
+
+     VectorBase(void* ptr, int n)
+     : m_AddressAligned(ptr), m_Length(n)
+     {
+         printf("%s\n", __func__);
+     }
+
+     ~VectorBase()
+     {
+         printf("%s\n", __func__);
+         printf("Aligned=%p\n", m_AddressAligned);
+     }
+
+
+     inline int GetLength(void) const { return m_Length; }
+
+private:
+     void* m_AddressAligned;
+     int m_Length;
+};
+
+class Vector : public VectorBase
+{
+public:
+     Vector(int n)
+     : VectorBase(n)
+     {
+         printf("%s\n", __func__);
+         const int align = 16;
+         m_AddressBase = new double[n+align-1];
+         unsigned char* ptr = reinterpret_cast<unsigned
+char*>(m_AddressBase);
+         while ( (reinterpret_cast<int>(ptr) % align) != 0) ptr++;
+         m_AddressAligned = ptr;
+         printf("Base=%p\n", m_AddressBase);
+         printf("Aligned=%p\n", m_AddressAligned);
+     }
+     ~Vector()
+     {
+         printf("%s\n", __func__);
+         printf("Base=%p\n", m_AddressBase);
+         delete[] m_AddressBase;
+     }
+private:
+     double* m_AddressBase;
+};
+
+void PrintLength(VectorBase& v)
+{
+     printf("Length=%d\n", v.GetLength());
+}
+
+int main(void)
+{
+     VectorBase base(4);
+     Vector vector(2);
+     PrintLength(base);
+     PrintLength(vector);
+
+     return 0;
+}
