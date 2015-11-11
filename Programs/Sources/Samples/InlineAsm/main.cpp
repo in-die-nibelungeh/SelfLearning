81ade0bea767c130ba33aae21b1eb4917175f4f6
Anonymous <Anonymous@61bdfd1a-bc71-e54a-bcc3-70c19e11aa58>
2015-11-11 22:11:34 +0900

    Corrected a specifier of "b"

--- Programs/Sources/Samples/InlineAsm/main.cpp
+++ Programs/Sources/Samples/InlineAsm/main.cpp
@@ -4,17 +4,17 @@
 int main(int argc, char* argv[])
 {
     const int iter = 10000000;
-    int a = 1, b;
+    int a = 1, b = 0;
     for ( int i = 0; i < iter; ++i )
     {
         __asm__ volatile(
         ".rept 1000\n"
         "  add %[a], %[b]\n"
         ".endr\n"
-        :[b]"=r"(b)
+        :[b]"+r"(b)
         :[a]"r"(a)
         );
     }
-    printf("a=%d, b=%d\n", a, b);
+    printf("a=%d, b=%d (%d)\n", a, b, static_cast<int>(iter * 1000L));
     return 0;
 }
