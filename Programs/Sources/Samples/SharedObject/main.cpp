a77b6b62ce5dc33c7b2b0c7d704b8399166febbd
Ryosuke Kanata <in.die.nibelungen@gmail.com>
2015-11-18 17:09:40 +0900

    Added an SharedObject sample

---
+++ Programs/Sources/Samples/SharedObject/main.cpp
@@ -0,0 +1,11 @@
+
+// Refer to
+//   * https://blog.katty.in/4350
+
+extern int so_test();
+
+int main(void)
+{
+    so_test();
+    return 0;
+}
---
+++ Programs/Sources/Samples/SharedObject/so.cpp
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int so_test()
+{
+    printf("so test\n");
+    return 0;
+}
