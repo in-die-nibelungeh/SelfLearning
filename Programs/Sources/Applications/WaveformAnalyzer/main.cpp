108ddb76267664c9e8ee412f50d6386f2c926ac0
Ryosuke Kanata <in.die.nibelungen@gmail.com>
2015-12-07 23:21:03 +0900

    * (feature/AddWaveformAnalysis) Added Calculating wav's energy (on top of spectrum)

---
+++ Programs/Sources/Applications/WaveformAnalyzer/main.cpp
@@ -0,0 +1,128 @@
+#include <string>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "status.h"
+#include "types.h"
+#include "debug.h"
+#include "mcon.h"
+#include "mfio.h"
+#include "masp.h"
+
+status_t Analyze(const char* inputFile);
+
+static void usage(void)
+{
+    printf("Usage: %s INPUT \n", "wanalyzer");
+    printf("\n");
+    printf("INPUT must be .wav files.\n");
+}
+
+int main(int argc, char* argv[])
+{
+    std::string input;
+    if ( argc < 2 )
+    {
+        usage();
+        return 0;
+    }
+    input = std::string(argv[1]);
+
+    LOG("Input: %s\n", input.c_str());
+    Analyze(input.c_str());
+
+    return 0;
+}
+
+status_t Analyze(const char* inputFile)
+{
+    mcon::Vector<double> input;
+    mfio::Wave wave;
+    std::string fbody;
+
+    // Input
+    {
+        mcon::Matrix<double> inputMatrix;
+        LOG("Loading input ... ");
+        status_t status = wave.Read(inputFile, inputMatrix);
+
+        if (NO_ERROR != status)
+        {
+            printf("An error occured during reading %s: error=%d\n", inputFile, status);
+            return -ERROR_CANNOT_OPEN_FILE;
+        }
+        if ( wave.GetNumChannels() > 1 )
+        {
+            LOG("%d-channel waveform was input.\n", wave.GetNumChannels());
+            LOG("Only left channel is used for estimation.\n");
+        }
+        LOG("Done\n");
+
+        input = inputMatrix[0];
+    }
+
+    LOG("[Input]\n");
+    LOG("    SamplingRate: %d\n", wave.GetSamplingRate());
+    LOG("    Length      : %d\n", input.GetLength());
+    LOG("\n");
+
+    // Energy
+    {
+        const size_t N = input.GetLength();
+        mcon::Matrix<double> energyRatio(2, N);
+        const double energy = sqrt(input.GetDotProduct(input));
+        LOG("    Energy      : %g\n", energy);
+        double sum = 0;
+        for ( uint k = 0; k < N; ++k )
+        {
+            sum += input[k] * input[k];
+            energyRatio[0][k] = sqrt(sum);
+            energyRatio[1][k] = sqrt(sum) / energy;
+        }
+        fbody  = std::string(inputFile);
+        fbody.erase( fbody.length()-4, 4);
+        const std::string ecsv("_energy.csv");
+
+        mfio::Csv csv(fbody + ecsv);
+        csv.Write(",,Enegy,Energy ratio\n");
+        csv.Write(energyRatio);
+        csv.Close();
+    }
+
+    {
+        LOG("\n");
+        mcon::Matrix<double> polar;
+
+        {
+            mcon::Matrix<double> complex;
+            masp::ft::Ft(complex, input);
+            masp::ft::ConvertToPolarCoords(polar, complex);
+        }
+        {
+            const int N = input.GetLength();
+            mcon::Matrix<double> matrix(4, N);
+
+            for ( int i = 0; i < N; ++i )
+            {
+                matrix[0][i] = 1.0 * i / N;
+            }
+            matrix[1] = polar[0];
+            matrix[2] = polar[1];
+            matrix[3] = input;
+
+            fbody  = std::string(inputFile);
+            fbody.erase( fbody.length()-4, 4);
+            const std::string ecsv("_spectrum.csv");
+
+            mfio::Csv csv(fbody + ecsv);
+            csv.Write(",,Gain[dB],Phase[rad],Pulse\n");
+            csv.Write(matrix);
+            csv.Close();
+        }
+        LOG("Done\n");
+    }
+    LOG("Finished.\n");
+    return NO_ERROR;
+}
