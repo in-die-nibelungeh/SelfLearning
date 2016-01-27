2e71332d066b680fafdc72747eade5429bcbe345
Ryosuke Kanata <in.die.nibelungen@gmail.com>
2016-01-28 07:39:43 +0900

    (feature/IntegrateVectordIntoVector) Added a sample

---
+++ Programs/Sources/Samples/TemplateSpecializing/main.cpp
@@ -0,0 +1,13 @@
+#include "Template.h"
+
+int main(void)
+{
+    Class<double> cd;
+    Class<int16_t> ci16;
+    Class<int> ci;
+    cd.Print();
+    ci16.Print();
+    ci.Print();
+    return 0;
+}
+
